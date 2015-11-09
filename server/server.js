310828550f2ec6032a862d1c7f460ed8b4e2e89a
Jan Florian Dietrich <jfd@daenen4.de>
2015-11-09 07:35:23 +0100

    refactor(node5): cleanup old assets, start adding server

--- server/server.js
+++ server/server.js
@@ -1,64 +1,31 @@
-var logger = require('koa-logger');
-var router = require('koa-router');
-var serve = require('koa-static');
-var compress = require('koa-compress');
-var fs = require('fs');
-var React = require('react');
-var Router = require('react-router').Router;
-var getRoutes = require('./routes');
-var indexHTML = fs.readFileSync(__dirname+'/../client/index.html').toString();
-var koa = module.exports = require('koa');
-var app = koa();
-
-const config = require('./config.js');
-
-app.use(compress({
-    filter: function (content_type) {
-        return /application|text|image\/svg\+xml/i.test(content_type)
-    },
-    threshold: 2048,
-    flush: require('zlib').Z_SYNC_FLUSH
-}));
-app.use(logger());
-app.use(serve(config.assetPath));
-app.use(router(app));
+import Hapi from 'hapi';
+import { renderToString } = 'react-dom/server'
+import { match, RoutingContext } from 'react-router'
+import routes from '../client/javascripts/routes.jsx'
 
 
-var renderApp = (req, cb) => {
-    var path = req.url;
-    var htmlRegex = /¡HTML!/;
-    var dataRegex = /¡DATA!/;
-    var layoutRegex = /¡LAYOUT-CLASS!/;
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
 
-    var router = Router.create({
-        routes: getRoutes(),
-        location: path,
-        onAbort: function (redirect) {
-            cb({redirect});
-        },
-        onError: function (err) {
-            console.log('Routing Error');
-            console.log(err);
-        }
-    });
-
-    router.run((Handler, state) => {
-        var html = React.renderToString(<Handler />);
-        var output = indexHTML.
-            replace(htmlRegex, html).
-            replace(layoutRegex, (Math.random() >= 0.5) ? 'is-beam' : '');
-        cb(null, output);
-    });
-};
-
-renderApp.bind(this);
-
-
-app.use(function *(){
-    renderApp(this.request, (error, html) => {
-        this.body = html;
-    });
+server.route({
+    method: 'GET',
+    path: '/{p*}',
+    handler: function (request, reply) {
+      console.log(request.path);
+      match({ routes, location: request.path }, (error, redirectLocation, renderProps) => {
+          if (error) {
+            reply(500, error.message);
+          } else if (redirectLocation) {
+            reply(302, redirectLocation.pathname + redirectLocation.search);
+          } else if (renderProps) {
+            reply(renderToString(<RoutingContext {...renderProps} />));
+          } else {
+            reply(404, 'Not found');
+          }
+        })
+      }
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
