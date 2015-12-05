3df3a042afd04896e6d58c3087ac74cfbe10d526
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-05 23:03:03 +0100

    improv(deployment): make deployment work with pm2

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
 
+// externalize node-modules to avoid problems with express and css-modules
 var nodeModules = {};
 fs.readdirSync('node_modules')
 .filter(function(x) {
@@ -19,23 +20,27 @@ const BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
 module.exports = {
   entry: APP_PATH,
   target: 'node',
+  // context: __dirname,
+  // node: {
+  //   __dirname: true,
+  // },
   resolve: {
     extensions: ['', '.js', '.jsx', '.styl'],
     moduleDirectories: [
       'app/client',
-      'app/server'
-    ]
+      'app/server',
+    ],
   },
   externals: nodeModules,
   output: {
     path: BUILD_PATH,
-    filename: 'server.js'
+    filename: 'server.js',
   },
   module: {
     loaders: [
       {
-          test: /\.jsx$/,
-          loaders: ['babel'],
+        test: /\.jsx$/,
+        loaders: ['babel'],
       },
       {
         test: /\.js$/,
@@ -57,9 +62,9 @@ module.exports = {
         // use css/locals for calculating the css-module styles on the server
         // @see https://github.com/webpack/css-loader/issues/59
         test: /\.styl$/,
-        loader: 'css/locals?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus'
-      }
-    ]
+        loader: 'css/locals?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus',
+      },
+    ],
   },
-  plugins: []
-}
+  plugins: [],
+};
