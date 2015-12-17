4176d2d8573e7e7228de18d0da1a72345722ef60
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-17 21:15:15 +0100

    feat(tests): Add babelrc-config, ignore svg-files for tests (load undefined)

--- client/test/setup.js
+++ client/test/setup.js
@@ -1,6 +1,11 @@
 import hook from 'css-modules-require-hook';
 import stylus from 'stylus';
 
+// Do not load svg-files in tests
+require.extensions['.svg'] = function() {
+    return;
+};
+
 hook({
   // reduce class-names to the one in the stylesheets for tests
   generateScopedName: '[local]',
