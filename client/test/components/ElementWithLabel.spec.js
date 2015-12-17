41d7ef7d2beba30ae010bbcba6a7036e9c2bd24e
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-17 18:35:49 +0100

    feat(tests): Add test-setup for css-modules

--- client/test/components/ElementWithLabel.spec.js
+++ client/test/components/ElementWithLabel.spec.js
@@ -1,11 +1,24 @@
-// import { render } from 'enzyme';
-// import { expect } from '../utils/expect';
-//
-// import Link from '../../javascripts/components/ElementWithLabel.jsx';
-//
-// describe('Component: <ElementWithLabel />', () => {
-//   it('renders an empty element', () => {
-//     const element = render(<ElementWithLabel />);
-//     expect(element.find('.text')).to.have.length(1);
-//   });
-// });
+import React from 'react';
+import { render } from 'enzyme';
+import { expect } from '../utils/expect';
+
+import ElementWithLabel from '../../javascripts/components/ElementWithLabel.jsx';
+
+describe('Component: <ElementWithLabel />', () => {
+  it('renders an empty element', () => {
+    const wrapper = render(<ElementWithLabel />);
+    expect(wrapper.find('.text')).to.have.length(1);
+    expect(wrapper.find('.text--label')).to.have.length(1);
+  });
+
+  it('renders a label if passed', () => {
+    const wrapper = render(<ElementWithLabel label='Foo' />);
+    expect(wrapper.find('.text--label').html()).to.equal('Foo:');
+  });
+
+  it('renders the children', () => {
+    const wrapper = render(<ElementWithLabel label='Foo'><span className='child'>Bar</span></ElementWithLabel>);
+    expect(wrapper.find('.text--label').html()).to.equal('Foo:');
+    expect(wrapper.find('.child').html()).to.equal('Bar');
+  });
+});
