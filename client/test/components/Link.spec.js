e458b1c89a6b8cf35632055240373baa69f8269d
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-17 18:13:53 +0100

    feat(tests): Switch to enzyme for testing

--- client/test/components/Link.spec.js
+++ client/test/components/Link.spec.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import { expect } from '../utils/expect';
-import { buildAndSelectElementByTag } from '../utils/helper';
+import { render } from 'enzyme';
 
 import Link from '../../javascripts/components/Link.jsx';
 
 describe('Component: Link', () => {
   it('renders an empty link', () => {
-    const subject = buildAndSelectElementByTag(<Link />, 'a');
+    const wrapper = render(<Link />);
 
-    expect(subject.getAttribute('class')).to.equal('link');
-    expect(subject.innerHTML).to.equal('');
+    expect(wrapper.find('a').hasClass('link')).to.be.true;
+    expect(wrapper.find('a').html()).to.equal('');
   });
 
   it('renders the correct href when url parameter is passed', () => {
-    const subject = buildAndSelectElementByTag(<Link url='http://www.example.org'></Link>, 'a');
+    const wrapper = render(<Link url='http://www.example.org'></Link>);
 
-    expect(subject.getAttribute('href')).to.equal('http://www.example.org');
+    expect(wrapper.find('a').attr('href')).to.equal('http://www.example.org');
   });
 
   it('renders the correct innerText', () => {
-    const subject = buildAndSelectElementByTag(<Link>Foo Bar</Link>, 'a');
+    const wrapper = render(<Link>Foo Bar</Link>);
 
-    expect(subject.innerHTML).to.equal('Foo Bar');
+    expect(wrapper.find('a').text()).to.equal('Foo Bar');
   });
 
   it('renders additional classes', () => {
-    const subject = buildAndSelectElementByTag(<Link className='bar'></Link>, 'a');
+    const wrapper = render(<Link className='bar'></Link>);
 
-    expect(subject.getAttribute('class')).to.equal('link bar');
+    expect(wrapper.find('a').attr('class')).to.equal('link bar');
   });
 
   it('renders a title-attribute', () => {
-    const subject = buildAndSelectElementByTag(<Link title='bar'></Link>, 'a');
+    const wrapper = render(<Link title='bar'></Link>);
 
-    expect(subject.getAttribute('title')).to.equal('bar');
+    expect(wrapper.find('a').attr('title')).to.equal('bar');
   });
 
   it('should expand arbitray other props on the element', () => {
-    const subject = buildAndSelectElementByTag(<Link data-fun='very nice'></Link>, 'a');
-    expect(subject.getAttribute('data-fun')).to.equal('very nice');
+    const wrapper = render(<Link data-fun='very nice'></Link>);
+    expect(wrapper.find('a').attr('data-fun')).to.equal('very nice');
   });
 });
