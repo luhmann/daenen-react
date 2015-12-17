8f8d3498a2278547024aa8bb2aed0673d0463d31
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-17 18:13:53 +0100

    feat(tests): Switch to enzyme for testing

---
+++ client/test/components/ElementWithLabel.spec.js
@@ -0,0 +1,11 @@
+// import { render } from 'enzyme';
+// import { expect } from '../utils/expect';
+//
+// import Link from '../../javascripts/components/ElementWithLabel.jsx';
+//
+// describe('Component: <ElementWithLabel />', () => {
+//   it('renders an empty element', () => {
+//     const element = render(<ElementWithLabel />);
+//     expect(element.find('.text')).to.have.length(1);
+//   });
+// });
--- client/test/components/MailtoLink.spec.js
+++ client/test/components/MailtoLink.spec.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import { expect } from '../utils/expect';
-import { isElement } from 'react-addons-test-utils';
-import { buildAndSelectElementByTag, buildAndSelectElementByClass } from '../utils/helper';
+import { render } from 'enzyme';
 
 import MailtoLink from '../../javascripts/components/MailtoLink.jsx';
 
 describe('Component: MailtoLink', () => {
   it('should render an empty element with no properties passed', () => {
-    const subject = buildAndSelectElementByTag(<MailtoLink />, 'span');
+    const subject = render(<MailtoLink />);
 
-    expect(subject.querySelectorAll('.link').length).to.equal(1);
-    expect(subject.querySelector('a').getAttribute('class')).to.equal('link');
+    expect(subject.find('.link')).to.have.length(1);
+    expect(subject.find('a').attr('class')).to.equal('link');
   });
 
   it('should render additional classes if passed', () => {
-    const subject = buildAndSelectElementByClass(<MailtoLink className='foo bar'/>, 'link');
-    console.log(subject.outerHTML, global.document.documentElement.outerHTML);
-    expect(subject.getAttribute('class')).to.equal('link foo bar');
+    const subject = render(<MailtoLink className='foo bar'/>);
+    expect(subject.find('a').attr('class')).to.equal('link foo bar');
   });
 
-  it('should render and munge a passed email as href', () => {
-    let adress = 'harry@example.com';
-    const subject = buildAndSelectElementByTag(<MailtoLink email={adress} />, 'a');
+  it('should render a passed email as mailto-href', () => {
+    let address = 'harry@example.com';
+    const subject = render(<MailtoLink email={address} />);
+    const link = subject.find('a');
 
-    expect(subject.getAttribute('href')).to.match(/mailto:.+/);
+    expect(link.attr('href')).to.equal(`mailto:${address}`);
+  });
+
+  it('should render a passed email as innerText', () => {
+    let address = 'harry@example.com';
+    const subject = render(<MailtoLink email={address} />);
+    const link = subject.find('a');
+
+    expect(link.text()).to.equal(address);
   });
 });
