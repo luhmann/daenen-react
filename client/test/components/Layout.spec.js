401fa8a39932fc0bb4dd56ba115dcbf9912d1ad1
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-19 21:07:34 +0100

    feat(tests): Add last tests

--- client/test/components/Layout.spec.js
+++ client/test/components/Layout.spec.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from '../utils/expect';
 
 import Layout from '../../javascripts/components/Layout.jsx';
 
 describe('Component: <Layout />', () => {
-  it('should contain a header element', () => {
-    const wrapper = render(<Layout />);
-    expect(wrapper.find('.header')).to.have.length(1);
+  it('should contain to wrapper elements', () => {
+    const wrapper = shallow(<Layout />);
+    expect(wrapper.find('.layout')).to.have.length(1);
+    expect(wrapper.find('.wrapper')).to.have.length(1);
   });
 
-  it('should contain a link to impress', () => {
-    const wrapper = render(<Layout />);
-    expect(wrapper.find('.header--logo')).to.have.length(1);
+  it('should contain a header element', () => {
+    const wrapper = shallow(<Layout />);
+    expect(wrapper.find('Header')).to.have.length(1);
+  });
+
+  it('should contain a footer element', () => {
+    const wrapper = shallow(<Layout />);
+    expect(wrapper.find('Footer')).to.have.length(1);
+  });
+
+  it('children should be rendered', () => {
+    const wrapper = shallow(<Layout><div className='subject'></div></Layout>);
+    expect(wrapper.find('.subject')).to.have.length(1);
   });
 });
