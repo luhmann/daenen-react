428fb8a78c2d59143b98f003d2e6617ac4bb915e
Jan Florian Dietrich <jfd@daenen4.de>
2015-12-20 14:42:22 +0100

    chore: cleanup unused files

--- app.js
+++
@@ -1,4 +0,0 @@
-/* this is a bootstrap file to install node-jsx which needs to be required before loading any file
- * that contains jsx-code */
-require('node-jsx').install({harmony: true});
-require('./server/server');
